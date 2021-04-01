b7496a389e396f5ed4aeaf9fa265770aba4163a5
Chloe12250 <79823557+Chloe12250@users.noreply.github.com>
2021-04-01 11:47:50 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+document.write("hello,world!");
+function sayhello(){
+  var response = prompt("What's your name");
+  alert("hello" + response + ",have a good day!");
+}
+function add(a,b){
+  return a+b;
+}
+console.log(add(20,30));
+console.log(add(120,130));
+
+function showTopic(){
+  var x = document.getElementById('demo');
+  x.style.fontSize = "25px";
+  x.style.color = "red";
+}
+var movies = ["Avengers","Caption Marvel","Black panther","Spiderman"];
+movies[0]="wonder woman";
+movies.push("Avengers");
+
+console.log(movies);
+
+
+var fruits = [ ];
+for (i=1; i< 10 ; i++){ fruits[i] = prompt("Enter fruit"); }
+alert("fruits contains " + fruits);
+var movieDetails = [ {title: "Avengers",   time:"12pm",   status: "available"}, {title: "Captain Marvel",   time:"2pm",   status: "unavailable"}, {title: "Black Panther",   time:"4pm",   status: "available"}, {title: "Spiderman",   time:"6pm",   status: "available"} ];
+for (var movie of movieDetails){   if (movie.status === "available"){     console.log("The movie " + movie.title + " plays at " + movie.time)   } else{     console.log("Sorry, the movie " + movie.title + " is sold out.")   } };
